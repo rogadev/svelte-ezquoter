681a88f482735565c1d1cbc63d8e3fc2a7582468
Roga <ryanroga@gmail.com>
2022-03-24 12:59:41 -0700

    Add Auth & Profile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
-	import './app.css';
+	//import { supabase } from '../supabase/client.js';
+	//import './app.css';
 </script>
 
 <div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,26 @@
 <script>
-	import CreatePricesheet from '../components/CreatePricesheet.svelte';
+	import { user } from '../stores/sessionStore.js';
+	import { supabase } from '../supabase/client.js';
+	import Auth from '../components/Auth.svelte';
+	import Profile from '../components/Profile.svelte';
+
+	user.set(supabase.auth.user());
+
+	supabase.auth.onAuthStateChange((_, session) => {
+		user.set(session.user);
+	});
 </script>
 
 <div>
-	<CreatePricesheet />
+	<h1>Dashboard</h1>
+	<ul>
+		<li>list</li>
+		<li>of</li>
+		<li>tasks</li>
+	</ul>
+	{#if $user}
+		<Profile />
+	{:else}
+		<Auth />
+	{/if}
 </div>
