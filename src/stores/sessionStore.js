c74f57b6eca336907dd670e6d7d9ce5a5702d555
Roga <ryanroga@gmail.com>
2022-03-24 15:23:32 -0700

    Update sessionStore.js
    Removes logging

--- src/stores/sessionStore.js
+++ src/stores/sessionStore.js
@@ -5,8 +5,7 @@ import { supabase } from '../supabase/client.js';
 export const user = writable(supabase.auth.user());
 
 /** Auth Setup */
-supabase.auth.onAuthStateChange((event, session) => {
-	console.log('Auth State Change:', event);
+supabase.auth.onAuthStateChange((_, session) => {
 	if (session) {
 		user.set(session.user);
 	} else {
