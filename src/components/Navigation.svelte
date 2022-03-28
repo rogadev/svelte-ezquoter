3f658dac177004fbb59bc3ec4b57e4013aacd4fc
vonroga <ryanroga@gmail.com>
2022-03-28 13:05:02 -0700

    Update Navigation.svelte
    Style change to remove false "active" class from showing. Will add back in when we can dynamically move the "active" class.

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -55,7 +55,7 @@
 							<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"-->
 							<a
 								href="/"
-								class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+								class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 								aria-current="page"
 								>Dashboard
 							</a>
