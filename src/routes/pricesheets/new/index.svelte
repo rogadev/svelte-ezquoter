fdf96e6206f7333a2bb58d92096c0a3c4391f2c4
vonroga <ryanroga@gmail.com>
2022-03-28 12:56:14 -0700

    Update New Pricesheet Page

--- src/routes/pricesheets/new/index.svelte
+++ src/routes/pricesheets/new/index.svelte
@@ -1,30 +1,40 @@
 <script>
+	import { fade } from 'svelte/transition';
+
+	import CancelModal from '../../../components/CancelModal.svelte';
+	import NewPricesheetForm from '../../../components/NewPricesheetForm.svelte';
+
 	let name;
 	let description;
+	let showCancelModal = false;
 
 	function handleSubmit() {
 		console.log(name, description, prices);
-		//
+		// TODO - Gather data and send to DB.
+	}
+
+	function handleCancel() {
+		toggleCancelModal();
+		history.back();
+		console.log('should have gone back');
+	}
+
+	function toggleCancelModal() {
+		// TODO - Build cancel modal.
+		showCancelModal = !showCancelModal;
 	}
 
 	$: prices = {
-		// Reactively calculate the prices
+		// TODO - Build prices component and pass props here.
 	};
 </script>
 
-<a
-	href="/pricesheets"
-	class="text-gray-100 bg-red-600 hover:bg-red-700 hover:text-white px-5 py-2 rounded-md text-sm font-medium"
-	><i class="fa-solid fa-xmark pr-2" /> Cancel</a
->
+<svelte:head>
+	<title>New Pricesheet</title>
+</svelte:head>
 
-<p>Create a new pricesheet</p>
+{#if showCancelModal}
+	<CancelModal on:cancel={toggleCancelModal} on:okay={handleCancel} />
+{/if}
 
-<form on:submit|preventDefault={handleSubmit}>
-	<label for="name">Name</label>
-	<input bind:value={name} id="name" type="text" name="name" />
-	<label for="description">Description</label>
-	<input bind:value={description} id="description" type="text" name="description" />
-	<input id="prices" name="prices" type="hidden" bind:value={prices} />
-	<button type="submit">Submit</button>
-</form>
+<NewPricesheetForm on:okay={handleSubmit} on:cancel={toggleCancelModal} />
